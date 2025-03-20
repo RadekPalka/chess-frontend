455b6163c3fe3e19c13cda26b8ab40e5fe6a2aea
Radosław Pałka <radekpalka@gmail.com>
2025-03-20 20:17:26 +0100

    fix: remove connection bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { RoomList } from './components/RoomList';
 import { Form } from './components/Form';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:3000');
+export type Room = {
+	name: string;
+	numberOfPlayers: number;
+};
 
 function App() {
 	const [userName, setUserName] = useState('');
+	const [rooms, setRooms] = useState<Room[]>([]);
 
 	const createUserName = (inputText: string) => {
 		if (inputText.length < 3) return;
@@ -15,8 +22,24 @@ function App() {
 		sessionStorage.setItem('username', userName);
 	}, [userName]);
 
+	useEffect(() => {
+		socket.on('updateRooms', (updatedRooms) => {
+			setRooms(updatedRooms);
+		});
+
+		return () => {
+			socket.off('updateRooms');
+		};
+	}, []);
+
 	return (
-		<>{userName === '' ? <Form createUserName={createUserName} /> : <RoomList />}</>
+		<>
+			{userName === '' ? (
+				<Form createUserName={createUserName} />
+			) : (
+				<RoomList userName={userName} rooms={rooms} socket={socket} />
+			)}
+		</>
 	);
 }
 
--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -1,38 +1,36 @@
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-const socket = io('http://localhost:3000')
-type Room = {
-  name: string;
-  numberOfPlayers: number;
-}
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
+import { Room } from '../App';
 
-export const RoomList: React.FC = () =>{
-    
-  const [rooms, setRooms] = useState<Room[]>([])
+type Props = {
+	userName: string;
+	rooms: Room[];
+	socket: Socket<DefaultEventsMap, DefaultEventsMap>;
+};
+export const RoomList: React.FC<Props> = ({ userName, rooms, socket }) => {
+	const joinRoom = (roomName: string) => {
+		socket.emit('joinRoom', roomName, userName);
+	};
 
-  useEffect(() => {
-    socket.on("updateRooms", (updatedRooms) => {
-      setRooms(updatedRooms);
-    });
-
-    return () => {
-      socket.off("updateRooms");
-    };
-  }, []);
-
-  const joinRoom = (roomName: string) =>{
-    socket.emit("joinRoom", roomName);
-}
-
-  const isRoomFull = (numberOfPlayers: number, roomName: string) =>{
-    return numberOfPlayers <2 ? <button onClick={()=> joinRoom(roomName)}>Join room</button> : <p>Room is full</p>
-  }
-  return (
-    <>
-    {rooms.length === 0 && <p>Czekam na połączenie z serwerem</p>} 
-    <ul>{rooms.map(room =><li>{room.name}: {room.numberOfPlayers} {isRoomFull(room.numberOfPlayers, room.name)}</li>)} </ul>
-    
-    
-    </>
-  )
-} 
+	const isRoomFull = (numberOfPlayers: number, roomName: string) => {
+		return numberOfPlayers < 2 ? (
+			<button onClick={() => joinRoom(roomName)}>Join room</button>
+		) : (
+			<p>Room is full</p>
+		);
+	};
+	return (
+		<>
+			{rooms.length === 0 && <p>Waiting for connection</p>}
+			<ul>
+				{rooms.map((room) => (
+					<li>
+						{room.name}: {room.numberOfPlayers}{' '}
+						{isRoomFull(room.numberOfPlayers, room.name)}
+					</li>
+				))}{' '}
+			</ul>
+		</>
+	);
+};
